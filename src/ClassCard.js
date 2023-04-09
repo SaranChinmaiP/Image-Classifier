fadfaba4984cfac09de4cf0ecf3665fcd332362e
Saran P <95578805+SaranChinmaiP@users.noreply.github.com>
2023-04-09 20:10:37 +0530

    Update ClassCard.js

--- src/ClassCard.js
+++ src/ClassCard.js
@@ -227,7 +227,7 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 900,
+  width: '65vw',
   bgcolor: "background.paper",
   border: "2px dotted #000",
   boxShadow: 24,
