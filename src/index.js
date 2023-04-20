bc7dc488d9887383e45d6b7806641863f93efa25
Saran P <95578805+SaranChinmaiP@users.noreply.github.com>
2023-04-20 17:34:41 +0530

    Final Commit
    Added Some Branding

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import MainApp from './MainApp';
+import Header from './header';
+import Footer from './Footer.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div className='head'>
-    <header>
-      <h1 className="header-1">Image Classification Project</h1>
-    </header>
-    </div>
+    
+   <Header></Header>
     <MainApp></MainApp>
+    <Footer></Footer>
   </React.StrictMode>
 );
